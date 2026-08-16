77b5a0f5812d229d6b4edc5a828c1678213623dc
mariollesta <llesta.mario@gmail.com>
2026-08-16 19:03:05 +0200

    feat: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import styles from './App.module.css'
-import BaseLayout from './layouts/BaseLayout'
-import TaskForm from './components/TaskForm'
-import TaskList from './components/TaskList'
-import ShareSection from './components/ShareSection'
-import useTasks from './hooks/useTasks'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Home'
 
 function App() {
-  const { tasks, todayTasks, tomorrowTasks, addTask, toggleCompleted, removeTask } = useTasks()
-
   return (
-    <BaseLayout>
-      <div className={styles.container}>
-        <TaskForm onAdd={addTask} />
-
-        <div className={styles.lists}>
-          <TaskList
-            title="Hoy"
-            tasks={todayTasks}
-            onToggle={toggleCompleted}
-            onRemove={removeTask}
-          />
-
-          <TaskList
-            title="Mañana"
-            tasks={tomorrowTasks}
-            onToggle={toggleCompleted}
-            onRemove={removeTask}
-          />
-        </div>
-
-        <ShareSection tasks={tasks} />
-      </div>
-    </BaseLayout>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import styles from './Home.module.css'
+import BaseLayout from '../layouts/BaseLayout'
+import TaskForm from '../components/TaskForm'
+import TaskList from '../components/TaskList'
+import ShareSection from '../components/ShareSection'
+import useTasks from '../hooks/useTasks'
+
+function HomePage() {
+  const { tasks, todayTasks, tomorrowTasks, addTask, toggleCompleted, removeTask } = useTasks()
+
+  return (
+    <BaseLayout>
+      <div className={styles.container}>
+        <TaskForm onAdd={addTask} />
+
+        <div className={styles.lists}>
+          <TaskList
+            title="Hoy"
+            tasks={todayTasks}
+            onToggle={toggleCompleted}
+            onRemove={removeTask}
+          />
+
+          <TaskList
+            title="Mañana"
+            tasks={tomorrowTasks}
+            onToggle={toggleCompleted}
+            onRemove={removeTask}
+          />
+        </div>
+
+        <ShareSection tasks={tasks} />
+      </div>
+    </BaseLayout>
+  )
+}
+
+export default HomePage
